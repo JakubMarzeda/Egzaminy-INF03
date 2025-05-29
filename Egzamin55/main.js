18436fa459ebd29b07da42b7b6ccbd9559826de0
JakubMarzeda <jakub.marzeda11@gmail.com>
2025-05-29 11:16:33 +0200

    feat: Finished 55 exam

---
+++ Egzamin55/main.js
@@ -0,0 +1,12 @@
+najechanie = (zdjecieId, zrodloZdjecia) => {
+  document.getElementById(zdjecieId).src = zrodloZdjecia;
+};
+
+opuszczenie = (zdjecieId, zrodloZdjecia) => {
+  document.getElementById(zdjecieId).src = zrodloZdjecia;
+};
+
+klikniecie = (zdjecieId, zrodloZdjecia) => {
+      document.getElementById(zdjecieId).src = zrodloZdjecia;
+    document.getElementById("main-img").src = zrodloZdjecia;
+};
